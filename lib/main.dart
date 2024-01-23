47335e01c5e7914befe32c922e687c29a979f1e0
Aleksandr Myroniuk <miron.alikc@gmail.com>
2024-01-22 16:15:10 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:themoviedb_app/theme/app_colors.dart';
+import 'package:themoviedb_app/widgets/auth/auth_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.mainDarkBlue,
+        ),
+      ),
+      home: const AuthWidget(),
+    );
+  }
+}
---
+++ lib/widgets/auth/auth_widget.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:themoviedb_app/theme/app_button_style.dart';
+
+class AuthWidget extends StatelessWidget {
+  const AuthWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Login to your account',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: ListView(
+        children: const [
+          _HeaderWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class _HeaderWidget extends StatelessWidget {
+  const _HeaderWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      fontSize: 16,
+      color: Colors.black,
+    );
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 25),
+          const _FormWidget(),
+          const SizedBox(height: 25),
+          const Text(
+            'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple.',
+            style: textStyle,
+          ),
+          const SizedBox(height: 5),
+          TextButton(
+            style: AppButtonStyle.linkButton,
+            onPressed: () {},
+            child: const Text('Register'),
+          ),
+          const SizedBox(height: 25),
+          const Text(
+            'If you signed up but didn `t get your verification email.',
+            style: textStyle,
+          ),
+          const SizedBox(height: 5),
+          TextButton(
+            style: AppButtonStyle.linkButton,
+            onPressed: () {},
+            child: const Text('Verify email'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FormWidget extends StatefulWidget {
+  const _FormWidget();
+
+  @override
+  State<_FormWidget> createState() => _FormWidgetState();
+}
+
+class _FormWidgetState extends State<_FormWidget> {
+  final _loginTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  String? errorText;
+  void _auth() {
+    final login = _loginTextController.text;
+    final password = _passwordTextController.text;
+    if (login == 'admin' && password == 'admin') {
+      errorText = null;
+    } else {
+      errorText = 'Не верный логин или пароль';
+    }
+    setState(() {});
+  }
+
+  void _resetPassword() {}
+
+  @override
+  Widget build(BuildContext context) {
+    const color = Color(0xFF01B4E4);
+    const textStyle = TextStyle(
+      fontSize: 16,
+      color: Color(0xFF212529),
+    );
+
+    const textFieldDecorator = InputDecoration(
+      border: OutlineInputBorder(),
+      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      isCollapsed: true,
+    );
+    final errorText = this.errorText;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (errorText != null) ...[
+          Text(
+            errorText,
+            style: const TextStyle(
+              fontSize: 17,
+              color: Colors.red,
+            ),
+          ),
+          const SizedBox(height: 20),
+        ],
+        const Text(
+          'Username',
+          style: textStyle,
+        ),
+        const SizedBox(height: 5),
+        TextField(
+          controller: _loginTextController,
+          decoration: textFieldDecorator,
+        ),
+        const SizedBox(height: 20),
+        const Text(
+          'Password',
+          style: textStyle,
+        ),
+        const SizedBox(height: 5),
+        TextField(
+          controller: _passwordTextController,
+          decoration: textFieldDecorator,
+          obscureText: true,
+        ),
+        const SizedBox(height: 15),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: _auth,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(color),
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                textStyle: MaterialStateProperty.all(
+                  const TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                ),
+                padding: MaterialStateProperty.all(
+                  const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 8,
+                  ),
+                ),
+              ),
+              child: const Text('Login'),
+            ),
+            const SizedBox(width: 30),
+            TextButton(
+              onPressed: _resetPassword,
+              style: AppButtonStyle.linkButton,
+              child: const Text('Reset password'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
