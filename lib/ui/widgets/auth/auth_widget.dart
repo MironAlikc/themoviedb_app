fac394c4d56d155b2265d9a147a6cb6ecdac3f84
Aleksandr Myroniuk <miron.alikc@gmail.com>
2024-01-24 02:44:51 -0800

    NotifierProvider

--- lib/ui/widgets/auth/auth_widget.dart
+++ lib/ui/widgets/auth/auth_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:themoviedb_app/domain/library/Inherited/provider.dart';
 import 'package:themoviedb_app/ui/theme/app_button_style.dart';
 import 'package:themoviedb_app/ui/widgets/auth/auth_model.dart';
 
@@ -74,7 +75,7 @@ class _FormWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final model = AuthProvider.read(context)?.model;
+    final model = NotifierProvider.read<AuthModel>(context);
     const textStyle = TextStyle(
       fontSize: 16,
       color: Color(0xFF212529),
@@ -133,7 +134,7 @@ class _AuthButtonWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final model = AuthProvider.watch(context)?.model;
+    final model = NotifierProvider.watch<AuthModel>(context);
     const color = Color(0xFF01B4E4);
     final onPressed =
         model?.canStartAuth == true ? () => model?.auth(context) : null;
@@ -169,7 +170,8 @@ class _ErrorMessageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final errorMessage = AuthProvider.watch(context)?.model.errorMessage;
+    final errorMessage =
+        NotifierProvider.watch<AuthModel>(context)?.errorMessage;
     if (errorMessage == null) return const SizedBox.shrink();
 
     return Padding(
