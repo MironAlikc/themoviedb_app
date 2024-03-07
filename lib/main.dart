191953ecb4abd49adcf6d478a35793bfb05b6133
Aleksandr Myroniuk <miron.alikc@gmail.com>
2024-03-07 22:40:59 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:themoviedb_app/domain/library/Inherited/provider.dart';
 import 'package:themoviedb_app/ui/widgets/app/my_app.dart';
-import 'package:themoviedb_app/ui/widgets/app/my_app_model.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final model = MyAppModel();
-  await model.checkAuth();
+void main() {
   const app = MyApp();
-  final widget = Provider(model: model, child: app);
-  runApp(widget);
+  runApp(app);
 }
